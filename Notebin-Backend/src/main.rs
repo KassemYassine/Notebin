d53db684df3a17f7299186874943fb55d3e4e37b
KassemYassine <kmy05@mail.aub.edu>
2025-06-10 12:40:20 +0300

    added cors middleware for frontend intregretion

--- Notebin-Backend/src/main.rs
+++ Notebin-Backend/src/main.rs
@@ -13,16 +13,20 @@ use dotenv::dotenv;
 use config::Config;            
 use db::init_db_pool; 
 use routes::app_router;
-
+use tower_http::cors::{CorsLayer, Any};      
+use axum::http::Method;                            // ← for allow_methods
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
-
+    let cors = CorsLayer::new()
+    .allow_origin(Any)                
+    .allow_methods([Method::GET, Method::POST, Method::OPTIONS]) 
+    .allow_headers(Any); 
     let config = Config::from_env()?;
     let pool = init_db_pool(&config.database_url).await?;
 
-    let app = app_router()
+    let app = app_router().layer(cors)
     .layer(Extension(pool));
 
     let addr = ([0, 0, 0, 0], config.port).into();
