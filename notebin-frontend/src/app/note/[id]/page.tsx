26cf0965afae4af27180123dc234b99d728b761a
KassemYassine <kmy05@mail.aub.edu>
2025-06-11 17:05:15 +0300

    added back button in notes/id page

--- notebin-frontend/src/app/note/[id]/page.tsx
+++ notebin-frontend/src/app/note/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { api } from "@/lib/api";
 
 type Note = {
@@ -32,6 +33,12 @@ export default async function NotePage({
 
   return (
     <div className="max-w-md mx-auto mt-10 space-y-4 p-6 bg-white rounded-lg shadow-md">
+      <Link
+        href="/"
+        className="inline-block px-4 py-2 bg-amber-300 text-gray-800 rounded-md hover:bg-amber-600 transition-colors"
+      >
+        ← 
+      </Link>
       <h1 className="text-lg font-bold text-gray-800">Note #{note.id}</h1>
 
       <div className="min-h-[200px]">
